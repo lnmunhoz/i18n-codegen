e711fb699bbaf67ac751aa8012040c4f2743f787
Forrest Wilkins <fwilkins@imagequix.com>
2022-01-26 13:57:19 -0500

    Filters for .defaultMessage, removed prettier-ignore line

--- src/core/codegen.ts
+++ src/core/codegen.ts
@@ -13,13 +13,15 @@ export const generateCode = (translations: TranslationsDict) => {
   const flat = flattenObject(translations);
   const keys = Object.keys(flat);
 
-  return `// prettier-ignore
-export const I18nKeys = [
-  ${keys.map(key => `"${key}"`)}
-] as const;
+  const code = `
+  export const I18nKeys = [
+    ${keys.map(key => `"${key}"`)}
+  ] as const;
+  
+  export type I18nKey = typeof I18nKeys[number];
+  `;
 
-export type I18nKey = typeof I18nKeys[number];
-`;
+  return code.replace('.defaultMessage', '');
 };
 
 export const runCodegen = async (config: I18nCodegenConfig) => {
